c2fa2a2713190c8065a162993235c39e565fe762
Holamite <olamideadebara@gmail.com>
2024-02-27 23:03:28 +0100

    fixed code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,17 +5,15 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.24",
   etherscan: {
-    apiKey: {
-      sepolia: "bc1v0w6TRKOeco-Bw3FaV2zLj-h6BxY1",
+    apiKey: process.env.API_KEY,
+  },
+  networks: {
+    sepolia: {
+      url: process.env.API_URL,
+      accounts: [process.env.PRIVATE_KEY],
     },
-    networks: {
-      sepolia: {
-        url: process.env.API_URL,
-        accounts: [process.env.PRIVATE_KEY],
-      },
-    },
-    sourcify: {
-      enabled: true,
-    },
+  },
+  sourcify: {
+    enabled: true,
   },
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -15,7 +15,15 @@ async function main() {
 
   const social = await ethers.deployContract("Social", [nftFactoryContract]);
 
-  console.log(`Contract deployed to ${nftFactory.target}`);
+  await social.waitForDeployment();
+
+  console.log(
+    `NFTContract deployed to ${nftFactory.target} and social contract deployed to ${social.target}`
+  );
+
+  // const nftFactoryAddr = "0xf8A729F76f15Ee87b7da8bD723d6Ba03Ee41386F";
+
+  // const socialFactoryAddr = "0x5670146a55A591Aa162Cea011667467e360D7Aa4";
 }
 
 // We recommend this pattern to be able to use async/await everywhere
